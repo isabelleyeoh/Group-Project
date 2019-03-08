1d0471e48d5e05383919990c8502aaf38f9d2e74
Jian Ming <jianmingchan@gmail.com>
2019-03-08 21:54:41 +0800

    Generate predicted custom concept and query this result in Product database. Create Product table

--- ml_web/util/helper_clarifai.py
+++ ml_web/util/helper_clarifai.py
@@ -11,28 +11,34 @@ app_clarifai=ClarifaiApp(api_key=app.config['CLARIFAI_API_KEY'])
 # Clarifai - predict chair
 def predict_model_chair(image_path, model,input_file, workflow_id):
 
-    # model = app_clarifai.models.get(model)
-
     # Add workflow
     workflow=Workflow(app_clarifai.api, workflow_id=workflow_id)
-
+    # Use either filename or URL
     if input_file==True:
         image = app_clarifai.inputs.create_image_from_filename(filename=image_path)
     else:
         image = app_clarifai.inputs.create_image_from_url(url=image_path)
-        
+    #JSON response 
     response_data = workflow.predict([image])
-    predict_by_url = []
 
-    breakpoint()
-    for concept in response_data['outputs'][0]['data']['concepts']:
-        if concept['value']>0: 
+    result_cust_model=[]
+    result_gen_model = []
+
+    for concept in response_data['results'][0]['outputs'][1]['data']['concepts']:
+        if concept['value']>0.90: 
             val = (concept['name'],concept['value'])
-            predict_by_url.append(val)
+            result_cust_model.append(val)
+        else:
+            pass
+    
+    for concept in response_data['results'][0]['outputs'][0]['data']['concepts']:
+        if concept['value']>0.90: 
+            val = (concept['name'],concept['value'])
+            result_gen_model.append(val)
         else:
             pass
 
-    return predict_by_url
+    return result_gen_model,result_cust_model
 
 
 
