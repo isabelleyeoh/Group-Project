37a998f5193d7d325359c6d4fbfb8c2f9f298eac
Isabelle Yeoh <yeoh.isabelle@gmail.com>
2019-03-08 17:16:27 +0800

    update links for html files

--- ml_web/blueprints/images/views.py
+++ ml_web/blueprints/images/views.py
@@ -43,14 +43,13 @@ def update(id):
 def predict_chair():
     input_file=False #True if using local path. False if using URL
     model='Next_Academy_Project'
-    image_path='https://www.ikea.com/gb/en/images/products/jokkmokk-chair-antique-stain__0475400_pe615581_s4.jpg'
+    image_path='https://www.ikea.com/my/en/images/products/antilop-highchair-with-tray-white__0339304_PE527619_S4.JPG'
     result = predict_model_chair(image_path=image_path, model=model, input_file=input_file)
 
     # for x,y in result:
     #     print(f'The {x} concept has a {y} probability of matching your input.')
 
     print(result)
-
     return  "pass"
     
     # if result != "":
--- ml_web/blueprints/sessions/views.py
+++ ml_web/blueprints/sessions/views.py
@@ -14,8 +14,15 @@ sessions_blueprint = Blueprint('sessions',
                             template_folder='templates')
 
 
+# @sessions_blueprint.route('/new', methods=['GET'])
+# def new():
+#     return render_template('sessions/option.html')
+
+
 @sessions_blueprint.route('/new/<usertype>', methods=['GET'])
-def new(usertype):
+def new(usertype):        
+    # if request.method == 'GET':
+    #     usertype = request.form['usertype']
     return render_template('sessions/new.html', usertype=usertype)
 
 
@@ -45,7 +52,6 @@ def login():
 
 @sessions_blueprint.route('/', methods=['POST'])
 def check():
-    username = request.form['username']
     email_to_check = request.form['email']
     password_to_check = request.form['password']
     user = Buyer.get_or_none(Buyer.email == email_to_check) or Seller.get_or_none(Seller.email == email_to_check)
