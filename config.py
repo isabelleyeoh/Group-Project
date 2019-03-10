8ab9b94ff9e3229d1b6b6cdabc8237b68a6b7516
Jian Ming <jianmingchan@gmail.com>
2019-03-10 16:03:15 +0800

    Pip install boto3 for Amazon AWS S3 Add AWS S3 reference to config.py

--- config.py
+++ config.py
@@ -12,6 +12,12 @@ class Config(object):
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
 
+    # JM added: AWS S3 configuration
+    S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+    S3_KEY = os.environ.get("S3_ACCESS_KEY")
+    S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+    S3_LOCATION = f'http://{S3_BUCKET}.s3.amazonaws.com/'
+
 class ProductionConfig(Config):
     DEBUG = False
     ASSETS_DEBUG = False
