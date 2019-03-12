fafb021ef3d8424c2b5833072b622b4c861d8388
Jian Ming <jianmingchan@gmail.com>
2019-03-12 18:55:28 +0800

    Minor amendments

--- ml_web/helpers/helper_clarifai.py
+++ ml_web/helpers/helper_clarifai.py
@@ -6,22 +6,19 @@ from flask import render_template, redirect, url_for
 
 
 app_clarifai=ClarifaiApp(api_key=app.config['CLARIFAI_API_KEY'])
-
+app_clarifai_search=ClarifaiApp(api_key=app.config['CLARIFAI_API_KEY_SEARCH'])
 
 # Clarifai - predict model
 def model_prediction(image_path, model,input_file, workflow_id):
 
     # Add workflow
     workflow=Workflow(app_clarifai.api, workflow_id=workflow_id)
-
-    # See what image is being created
-    # app_clarifai.inputs.create_image_from_url(url=image_path)
-
+    # Predict using clarifai
     response_data = workflow.predict_by_url(url=image_path)
 
     result_cust_model=[]
     result_gen_model = []
-    ref_list=['chair','table','sofa','armchair']
+    ref_list=['chair','table','sofa','armchair', 'lamp', 'bookcase','shelf']
 
     # Custom Model: Append results and return a list
     for concept in response_data['results'][0]['outputs'][1]['data']['concepts']:
@@ -34,14 +31,13 @@ def model_prediction(image_path, model,input_file, workflow_id):
     # General Model:
 
     for concept in response_data['results'][0]['outputs'][0]['data']['concepts']:
-        if concept['value']>0.80 and concept['name']in ref_list:
+        if concept['value']>0.60 and concept['name']in ref_list:
 
             val = (concept['name'],concept['value'])
             result_gen_model.append(val)
         else:
             pass
 
-    
     return result_cust_model, result_gen_model
 
 
