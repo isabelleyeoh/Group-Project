f8b29d22c100805f4dbc3f6e41cad45538f6a78b
Jian Ming <jianmingchan@gmail.com>
2019-03-14 09:59:01 +0800

    Deleted unused code

--- ml_web/blueprints/images/views.py
+++ ml_web/blueprints/images/views.py
@@ -17,11 +17,11 @@ images_blueprint = Blueprint('images',
                             __name__,
                             template_folder='templates')
 
-
+# Upload new image
 @images_blueprint.route('/new', methods=['GET'])
 def new():
 
-    pass
+    return render_template('images/uploader.html')
 
 
 @images_blueprint.route('/', methods=['POST'])
@@ -48,92 +48,3 @@ def edit(id):
 @images_blueprint.route('/<id>', methods=['POST'])
 def update(id):
     pass
-
-# *********JM TESTING UPLOAD OF IMAGE TO RUN THROUGH CLARIFAI **************#
-
-@images_blueprint.route('/uploader', methods=['GET'])
-def image_uploader():
-
-    return render_template('images/uploader.html')
-
-@images_blueprint.route('/search_result', methods=['POST'])
-def search_result():
-    # A: Check if there is file in form
-    
-    if "search_image" not in request.files:
-        return "No user_file key in request.files"
-
-	# B
-    file    = request.files["search_image"]
-    # print(file)
-
-	# C.
-    if file.filename == "":
-        return "Please select a file"
-
-	# D.
-    if file and allowed_file(file.filename):
-
-        # Ensure correct orientation
-        
-        img = PILImage.open(file) #Create a Pillow file object
-
-        if hasattr(img, '_getexif'):
-            exifdata = img._getexif()
-            try:
-                orientation = exifdata.get(274)
-                print(orientation)
-                if orientation==6:  
-                    img = img.rotate(-90)
-                else:
-                    pass
-            except:
-                pass
-                # orientation = 1
-        
-        fs = BytesIO()
-        # Save image with Pillow into FileStorage object.
-        img.save(fs, format='JPEG')
-
-        file.filename = secure_filename(file.filename)
-        
-        output = upload_file_to_s3(fs, file.filename, app.config["S3_BUCKET"], file.mimetype)
-        image_url=output
-        
-        # Run image through Clarifai
-
-        input_file=False #True if using local path. False if using URL
-        model='Next_Academy_Project'
-        workflow_id="Furniture-1"
-        image_path=image_url #Get image url from AWS S3
-        result = model_prediction(image_path=image_path, model=model, input_file=input_file, workflow_id=workflow_id)
-       
-        # Define: Custom Model and Search Model tuple list
-
-        cust_result_list=result[0]
-        search_result_list=result[1]
-
-        # Check: Exact Product Match
-        if cust_result_list[0][1]>0.70:
-            # Get matching model + recommended items
-            matching_products = Product.get_or_none(Product.concept==cust_result_list[0][0])
-            search_result_list=search_result_list[:5]
-            # Check: Top 3 similar Products but omit matching model
-            similar_products= Product.select().where(Product.clarifai_id << search_result_list, Product.clarifai_id!=matching_products.clarifai_id)
-            match = True
-            
-        else:
-            # Recomend items based on furniture type
-            matching_products=""
-            search_result_list=search_result_list[:4]
-            # Check: Top 3 similar Products but omit matching model
-            similar_products= Product.select().where(Product.clarifai_id << search_result_list)
-
-            match = False
-
-
-        # Render: template showing product result
-        return render_template("images/buyer.html", cust_result_list=cust_result_list,file_name=file.filename, match = match, matching_products=matching_products,similar_products=similar_products,search_image=image_url)
-
-
-        
